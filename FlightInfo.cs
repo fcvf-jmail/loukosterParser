3e5e031dd5efc579b17a137ca881569f8044927a
Jmail El Kazzaz <gamelelkazzaz@gmail.com>
2024-08-14 02:14:33 +0300

    added screenshot

--- FlightInfo.cs
+++
@@ -1,6 +0,0 @@
-namespace LoukosterParser;
-class FlightInfo (string url, decimal maxPrice)
-{
-    public string Url { get; private set; } = url;
-    public decimal MaxPrice { get; private set; } = maxPrice;
-}
